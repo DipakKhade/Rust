9a7f27055e165fa627091b138199e57b0fc5b2e1
Dipak Khade <137380836+DipakKhade@users.noreply.github.com>
2024-09-11 23:19:45 +0530

    Delete rust-init/target directory

--- rust-init/target/debug/deps/rust_init-3f78fcdac06e76f6.d
+++
@@ -1,5 +0,0 @@
-D:\Rust\Rust\rust-init\target\debug\deps\librust_init-3f78fcdac06e76f6.rmeta: src\main.rs
-
-D:\Rust\Rust\rust-init\target\debug\deps\rust_init-3f78fcdac06e76f6.d: src\main.rs
-
-src\main.rs:
