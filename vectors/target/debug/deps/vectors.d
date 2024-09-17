07661a8bbab7bdd1820cee50c4a646a2f4cb7618
= <=>
2024-09-17 11:41:35 +0530

    rust vectors

---
+++ vectors/target/debug/deps/vectors.d
@@ -0,0 +1,5 @@
+D:\Rust\Rust\vectors\target\debug\deps\vectors.exe: src\main.rs
+
+D:\Rust\Rust\vectors\target\debug\deps\vectors.d: src\main.rs
+
+src\main.rs:
---
+++ vectors/target/debug/vectors.d
@@ -0,0 +1,1 @@
+D:\Rust\Rust\vectors\target\debug\vectors.exe: D:\Rust\Rust\vectors\src\main.rs
