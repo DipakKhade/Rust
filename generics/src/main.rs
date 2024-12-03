4f07f2ac6d16274b3783ac8d6664686fd9561216
Dipak Khade <dipakhade214@gmail.com>
2024-12-03 22:39:11 +0530

    generic exm

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -5,8 +5,19 @@ fn main() {
         Some(data) => println!("{}", data),
         None => println!("unable to find the first element"),
     }
+
+    let rollno = vec![1, 2, 43, 230];
+    let last_roll_no = get_last_element(&rollno);
+    match last_roll_no {
+        Some(data) => println!("The last element of vector is {}", data),
+        None => println!("unable to find the last element"),
+    }
 }
 
 fn get_first_element<T: std::cmp::PartialOrd>(a: &Vec<T>) -> Option<&T> {
     a.get(0)
+}
+
+fn get_last_element<T: std::cmp::PartialOrd>(a: &Vec<T>) -> Option<&T> {
+    a.get(a.len() - 1)
 }
