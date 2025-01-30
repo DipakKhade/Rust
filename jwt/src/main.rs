d798c6116bc079697a8e8517cce90774b7d5e782
Dipak Khade <dipakhade214@gmail.com>
2025-01-30 17:57:20 +0530

    init

---
+++ jwt/src/main.rs
@@ -0,0 +1,19 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[post("/login")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
