d40af02ce81e5893aabf641de5b48a9d71d88068
Dipak Khade <dipakhade214@gmail.com>
2025-02-04 21:27:41 +0530

    macros custom

--- Crust-of-Rust-macros/src/lib.rs
+++ Crust-of-Rust-macros/src/lib.rs
@@ -4,7 +4,50 @@ macro_rules! avec {
     };
 }
 
+#[warn(dead_code)]
 fn foo() {
     let mut x = 2;
     avec!(x);
+}
+
+// micros in rust library
+//
+
+macro_rules! four {
+    () => {
+        1 + 3
+    };
+}
+
+macro_rules! capture_then_match_tokens {
+    ($e:expr) => {
+        match_tokens!($e)
+    };
+}
+
+macro_rules! match_tokens {
+    ($a:tt + $b:tt) => {
+        "got an addition"
+    };
+    (($i:ident)) => {
+        "got an identifier"
+    };
+    ($($other:tt)*) => {
+        "got something else"
+    };
+}
+
+fn main() {
+    println!(
+        "{}\n{}\n{}\n",
+        match_tokens!((caravan)),
+        match_tokens!(3 + 6),
+        match_tokens!(5),
+    );
+    println!(
+        "{}\n{}\n{}",
+        capture_then_match_tokens!((caravan)),
+        capture_then_match_tokens!(3 + 6),
+        capture_then_match_tokens!(5),
+    );
 }
