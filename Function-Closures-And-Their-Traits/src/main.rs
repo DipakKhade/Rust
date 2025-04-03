320c93787453576789bcc990ae7b8c3bff12aed8
Dipak Khade <dipakhade214@gmail.com>
2025-04-03 16:48:54 +0530

    closure move

--- Function-Closures-And-Their-Traits/src/main.rs
+++ Function-Closures-And-Their-Traits/src/main.rs
@@ -4,8 +4,18 @@ fn main() {
     let y = |x: i32, y: i32| x + y;
     let sum = y(2, 4);
     println!("sum is :{}", sum);
+
+    let mut a = 12;
+
+    let f = move || {
+        drop(a);
+    };
 }
 
 fn bar() {}
 
 fn baz(f: fn(u32) -> u32) {}
+
+fn hello(f: Box<dyn Fn()>) {
+    f()
+}
