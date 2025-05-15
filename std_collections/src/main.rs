8278ab11c310ddc39201969059843e849b28029b
Dipak Khade <dipakhade214@gmail.com>
2025-05-15 17:08:51 +0530

    vec dque

--- std_collections/src/main.rs
+++ std_collections/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let mut v = std::collections::VecDeque::with_capacity(10);
+    v.push_back(2);
+    println!(" vec q queue {:?}", v);
+    // v.append(other);
+    // v.split_off(at)
+    // v.as_mut_slices()
+    // v.back()
+    // v.back_mut()
+    // v.binary_search(x)
 }
