dc7376759cf235954ae9e748d05ada6e2f7dc555
Dipak Khade <137380836+DipakKhade@users.noreply.github.com>
2025-06-17 16:27:39 +0530

    added post models.rs

--- diesel/db/src/models.rs
+++ diesel/db/src/models.rs
@@ -11,3 +11,13 @@ pub struct User {
     pub password: String,
     pub is_admin: bool
 }
+
+#[derive(Queryable, Selectable)]
+#[diesel(table_name = crate::schema::users)]
+#[diesel(check_for_backend(diesel::mysql::Mysql))]
+pub struct Post {
+    pub id: i32,
+    pub title: String,
+    pub createdate: String,
+    pub createdby:  i32
+}
