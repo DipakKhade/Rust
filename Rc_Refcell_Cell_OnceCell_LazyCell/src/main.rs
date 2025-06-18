ecdd3d89132644b91605fc5ee0f1c04382d475b8
Dipak Khade <137380836+DipakKhade@users.noreply.github.com>
2025-06-18 21:57:45 +0530

    cell

--- Rc_Refcell_Cell_OnceCell_LazyCell/src/main.rs
+++ Rc_Refcell_Cell_OnceCell_LazyCell/src/main.rs
@@ -1,32 +1,36 @@
-use std::rc::Rc;
+use std::{cell, rc::Rc};
 
 #[derive(Debug)]
 struct Node {
-    pub id: i32,
+    pub id: cell::Cell<i32>,
     pub points: Option<Rc<Node>>,
 }
 
 fn main() {
     //RC
     //https://doc.rust-lang.org/rust-by-example/std/rc.html
+
+    //Cell
+    //https://doc.rust-lang.org/std/cell/
+
     let node1 = Rc::new(Node {
-        id: 1,
+        id: cell::Cell::new(1),
         points: None,
     });
 
     let node2 = Rc::new(Node {
-        id: 2,
+        id: cell::Cell::new(2),
         points: Some(Rc::clone(&node1)),
     });
 
     let node3 = Rc::new(Node {
-        id: 3,
+        id: cell::Cell::new(3),
         points: Some(Rc::clone(&node1)),
     });
 
     dbg!(Rc::strong_count(&node1));
 
-    //Cell
-    //https://doc.rust-lang.org/std/cell/
-
+    cell::Cell::set(&node1.id, 10);
+    
+    dbg!(node1);
 }
