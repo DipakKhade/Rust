abceae944c2779ecb911ac29fc5a848197a0b77b
Dipak Khade <137380836+DipakKhade@users.noreply.github.com>
2025-07-01 15:43:33 +0530

    chrono

--- chrono_create/src/main.rs
+++ chrono_create/src/main.rs
@@ -1,5 +1,22 @@
+use std::ops::Sub;
+
 
 fn main() {
     let d = std::time::Duration::from_secs(3);
-    println!("{:?}", d);
+    println!("{d:?}");
+
+    let sub_d = d.sub(std::time::Duration::from_secs(1));
+    println!("{sub_d:?}");
+
+    let start_time = std::time::Instant::now();
+    println!("{start_time:?}");
+
+    std::thread::sleep(std::time::Duration::from_secs(1));
+
+    let end_time = start_time.elapsed();
+    println!("{end_time:?}");
+
+    let todays_date = chrono::Utc::now();
+    println!("date is -- {todays_date}");
+
 }
